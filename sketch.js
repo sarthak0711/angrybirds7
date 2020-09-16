686604932607a32cde085ce1d111e22eec496319
sarthak0711 <68766980+sarthak0711@users.noreply.github.com>
2020-09-16 15:02:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ function keyPressed(){
 }
 
 async function getTime(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var rData = await response.json();
     var rDateTime = rData.datetime;
     var hourInfo = rDateTime.slice(11,13)
